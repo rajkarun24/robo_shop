16ca1eca14fdc742516ad6d9d3092c168f0aad4d
Raj <rajkarun0204@gmail.com>
2026-07-04 15:19:48 +0530

    change mongod to mongpdb

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-systemctl restart mongod
+systemctl enable mongodb
+systemctl start mongodb
+systemctl restart mongodb
