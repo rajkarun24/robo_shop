936df6fa798383bd495dbe5e62d09bc10d704c90
Raj <rajkarun0204@gmail.com>
2026-07-09 20:06:36 +0530

    Correcting the Catalogue_sh file

--- catalogue.sh
+++ catalogue.sh
@@ -1,36 +1,39 @@
-echo -e "\e[36m>>>>>>>> copying catalogue service <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[36m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[36m>>>>>>>> copying mongo repo <<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
+yum install nodejs -y
 
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-
-echo -e "\e[36m>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
-dnf install nodejs -y
-
-echo -e  "\e[36m>>>>>>>> Add application User <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
 useradd roboshop
 
-echo -e "\e[36m>>>>>>>> Download the application code to created app dir <<<<<<<<\e[0m"
-rm -rf /app
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
 mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
+curl -o/tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
 
-echo -e "\e[36m>>>>>>>> Download dependencies <<<<<<<<\e[0m"
-cd \app
+echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
 npm install
 
-echo -e "\e[36m>>>>>>>> Load and start the catalogue service <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[36m>>>>>>>>> Start Catalogue Servie <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
-echo -e "\e[36m>>>>>>>> Install MongoDB Client <<<<<<<<\e[0m"
-dnf install mongodb-org-shell -y
+echo -e "\e[36m>>>>>>>>> Copy Mongo repo file <<<<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[36m>>>>>>>> Load the Schema <<<<<<<<\e[0m"
-mongo --host mongodb.rajkarun0606.online </app/schema/catalogue.js
+echo -e "\e[36m>>>>>>>>> Install MongoDB client <<<<<<<<<<\e[0m"
+yum install mongodb-org-shell -y
+
+echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.rdevopsb72.online </app/schema/catalogue.js
+I
