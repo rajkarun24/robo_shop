71bbecaa673afef75d1956981a8574432f288cd0
Raj <rajkarun0204@gmail.com>
2026-07-09 15:23:55 +0530

    printing statements for FE

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,18 @@
+echo -e "\e[36m>>>>>>>> Install Ngnix <<<<<<<<\e[0"
 dnf install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>> copying roboshop_conf <<<<<<<<\e[0"
+cp /Users/raj/Raghu_DevOps/robo_shop/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m>>>>>>>> Download the frontend content <<<<<<<<\e[0"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m>>>>>>>> Extracting frontend content <<<<<<<<\e[0"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>> Restart Nginx Service <<<<<<<<\e[0"
 systemctl restart nginx
 systemctl enable nginx
